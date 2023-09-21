34e88ec1f9facebaa7747bc91724414f267d6bb7
NikitaGudinHomePC <nikitagudin.ads@gmail.com>
2023-09-21 20:44:40 +0300

    main page markup is done

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import banner from '../img/banner.jpg'
+import TopSales from './TopSales'
+import Catalog from './Catalog'
 
 export default function MainPage() {
   return (
     <main className='container'>
-
+      <div className='row'>
+        <div className='col'>
+          <div className='banner'>
+            <img src={banner} className='img-fluid' alt='К весне готовы!' />
+            <h2 className='banner-header'>К весне готовы!</h2>
+          </div>
+          <TopSales />
+          <Catalog />
+        </div>
+      </div>
     </main>
   )
 }
